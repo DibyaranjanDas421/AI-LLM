78807e6fbe080fa22949a151a6d2f152b5eb3732
DibyaranjanDas421 <dibyaranjandas468@gmail.com>
2026-08-10 21:28:35 +0530

    completed list and touple assignments

---
+++ Python/04_List-and-Touple/assisgments/01_assignment.py
@@ -0,0 +1,20 @@
+fruits=[]
+
+fruit1 = input("Enter fruit 1: ")
+fruit2 = input("Enter fruit 2: ")
+fruit3 = input("Enter fruit 3: ")
+fruit4 = input("Enter fruit 4: ")
+fruit5 = input("Enter fruit 5: ")
+fruit6 = input("Enter fruit 6: ")
+fruit7 = input("Enter fruit 7: ")
+
+
+fruits.append(fruit1)
+fruits.append(fruit2)
+fruits.append(fruit3)
+fruits.append(fruit4)
+fruits.append(fruit5)
+fruits.append(fruit6)
+fruits.append(fruit7)
+
+print(fruits)
---
+++ Python/04_List-and-Touple/assisgments/02_assignment.py
@@ -0,0 +1,22 @@
+marks=[]
+
+mark1 = input("Enter mark 1: ")
+mark2 = input("Enter mark 2: ")
+mark3 = input("Enter mark 3: ")
+mark4 = input("Enter mark 4: ")
+mark5 = input("Enter mark 5: ")
+mark6 = input("Enter mark 6: ")
+mark7 = input("Enter mark 7: ")
+
+
+marks.append(mark1)
+marks.append(mark2)
+marks.append(mark3)
+marks.append(mark4)
+marks.append(mark5)
+marks.append(mark6)
+marks.append(mark7)
+
+marks.sort()
+
+print(marks)
